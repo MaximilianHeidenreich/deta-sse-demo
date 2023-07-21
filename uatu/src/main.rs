65f18ec0d1c29949869aff631c4d5ce4c067e297
Omer Faruk Bayram <omer@deta.sh>
2023-07-21 17:50:47 +0300

    add uatu to the repo
    Signed-off-by: Omer Faruk Bayram <omer@deta.sh>

---
+++ uatu/src/main.rs
@@ -0,0 +1,119 @@
+use axum::{
+    extract::{Path, State},
+    http::{Method, Response, StatusCode},
+    response::sse::{Event, KeepAlive, Sse},
+    routing::{get, post},
+    Router,
+};
+use futures_util::stream::Stream;
+use std::{env, sync::Arc};
+use tokio::sync::broadcast;
+use tokio_stream::{
+    wrappers::{errors::BroadcastStreamRecvError, BroadcastStream},
+    StreamExt as _,
+};
+use tower_http::cors::{Any, CorsLayer};
+
+#[derive(Debug, Default, Clone)]
+struct Data {
+    id: String,
+    data: String,
+}
+
+impl Data {
+    fn new(id: String, data: String) -> Self {
+        Self { id, data }
+    }
+}
+
+#[derive(Debug)]
+struct AppState {
+    tx: broadcast::Sender<Data>,
+    rx: broadcast::Receiver<Data>,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        let (tx, rx) = broadcast::channel(1024 * 10);
+        Self { tx, rx }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::HEAD])
+        .allow_origin(Any);
+
+    let adr = format!("0.0.0.0:{}", env::var("PORT").unwrap_or("8080".into()));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/sse/:id", get(sse_handler))
+        .route("/push/:id", post(push))
+        .layer(cors)
+        .with_state(Arc::new(AppState::default()));
+
+    println!("[INFO ] serving on {adr:?}");
+
+    axum::Server::bind(&adr.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    r#"
+POST /push/<id>
+HEAD /sse/<id>
+"#
+}
+
+async fn push(
+    Path(id): Path<String>,
+    State(state): State<Arc<AppState>>,
+    body: String,
+) -> Response<String> {
+    println!("[INFO ] new push: {id}:{body}");
+    let mut resp = Response::default();
+    let mut data = Data::new(id, body);
+
+    while state.tx.receiver_count() > 0 {
+        match state.tx.send(data) {
+            Ok(n) => {
+                if n != state.tx.receiver_count() {
+                    println!("[INFO ] pushed to: {n} receivers");
+                }
+                *resp.body_mut() = "ok".into();
+                *resp.status_mut() = StatusCode::OK;
+                break;
+            }
+
+            Err(ret) => {
+                data = ret.0;
+                // *resp.body_mut() = format!("internal error: {err}");
+                // *resp.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            }
+        };
+    }
+    resp
+}
+
+async fn sse_handler(
+    Path(id): Path<String>,
+    State(state): State<Arc<AppState>>,
+) -> Sse<impl Stream<Item = Result<Event, BroadcastStreamRecvError>>> {
+    println!("[INFO ] new subscriber: {id}");
+
+    Sse::new(
+        BroadcastStream::new(state.tx.subscribe())
+            .filter(move |data| match data {
+                Ok(Data { id: push_id, .. }) => *push_id == id,
+                Err(err) => {
+                    println!("{err}");
+                    true
+                }
+            })
+            .map(|data| data.map(|data| Event::default().data(data.data))),
+    )
+    .keep_alive(KeepAlive::default())
+}
